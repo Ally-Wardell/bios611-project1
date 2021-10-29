8a79aaca222e814a79ed56eedbf2205bf89132a4
Ally-Wardell <77655459+Ally-Wardell@users.noreply.github.com>
2021-10-29 15:16:04 -0400

    changed filepathways

--- derived_heart.R
+++ derived_heart.R
@@ -3,12 +3,12 @@
 
 library('tidyverse')
 
-heart_data <- read_csv("~/bios611/source_data/heart_data.csv",  na=c("", "NA", "?"))
+heart_data <- read_csv("source_data/heart_data.csv",  na=c("", "NA", "?"))
 
 derived_heart <- heart_data %>%
   mutate(Target=factor(ifelse(Target>1, 1, Target))) %>%
   #select(-`...1`) %>%
   drop_na()
 
-write_csv(derived_heart, "~/bios611/derived_data/derived_heart.csv" )
+write_csv(derived_heart, "derived_data/derived_heart.csv" )
 
