e6089981edb6b35b9355ca93486db52e426576d6
Ally-Wardell <77655459+Ally-Wardell@users.noreply.github.com>
2021-11-30 22:51:26 -0500

    updated titles

--- analysis_rf.R
+++ analysis_rf.R
@@ -11,69 +11,70 @@ library('e1071')
 derived_heart <- read_csv("~/bios611/derived_data/derived_heart.csv") %>%
   mutate(Target=factor(Target))
 
-individualfolds<- createFolds(y=derived_heart$Target, k=5)
-per_class_accuracy <- list()
-for(i in 1:length(individualfolds)){
-  heart_train <- derived_heart[-individualfolds[[i]],]
-  heart_test <- derived_heart[individualfolds[[i]],]
+total_p <- dim(derived_heart)[2]-1
+tuning_grid <- expand.grid("trees"=c(50, 250, 500),
+                           "p"=c(total_p/2, sqrt(total_p), total_p))
+test_results <- list()
+fold_k <- 5
+tt_indices <- createFolds(y = derived_heart$Target, k=fold_k)
+# Train, tune, test
+for(i in 1:length(tt_indices)){
   
-  # Try different number of predictors at split
-  reg_rf_preds_tune <- list() 
-  reg_rf_oob_error <- list()
-  tree_num <- c(50, 250, 500)
-  # There are 29 predictors
-  m_num <- c( (6/2), 6)
+  # Create train, test sets
+  heart_train <-derived_heart[-tt_indices[[i]],]
+  heart_test <-derived_heart[tt_indices[[i]],]
   
-  counter <- 1
-  for(h in 1:length(tree_num)){
-    for(j in 1:length(m_num)){
-      reg_rf_preds_tune[[counter]] <- randomForest(Target~., heart_train,
-                                                   ntree=tree_num[h], mtry=m_num[j])
-      reg_rf_oob_error[[counter]] <-
-        data.frame("tree_size"=tree_num[h],
-                   "num_pred" =m_num[j],
-                   "oob_error"=reg_rf_preds_tune[[counter]]$err.rate[tree_num[h]])
-      counter <- counter+1
-    }
+  # Tune over grid
+  tune_results <- c()
+  for(j in 1:dim(tuning_grid)[1]){
+    set.seed(12)
+    rf_tune <- randomForest(Target~., data=heart_train,
+                            mtry = tuning_grid$p[j],
+                            ntree = tuning_grid$trees[j])
+    tune_results[j] <- rf_tune$err.rate[tuning_grid$trees[j], 1]
   }
   
-  reg_rf_oob_error_df <- do.call("rbind", reg_rf_oob_error)
-  reg_rf_oob_error_df
+  train_tune_results <- cbind(tuning_grid, "oob_error"=tune_results)
+  best_tune <- train_tune_results[which(tune_results==min(tune_results)),][1,]
   
-  # Refit on training using best no. of predictors at split
-  best_error <- which(reg_rf_oob_error_df$oob_error==min(reg_rf_oob_error_df$oob_error))
-  reg_rf <- randomForest(Target~., heart_train,
-                         ntree=reg_rf_oob_error_df$tree_size[best_error], mtry=reg_rf_oob_error_df$num_pred[best_error])
+  # Fit on training use best tune
+  set.seed(12)
+  rf_fit <- randomForest(Target~., data=heart_train,
+                         mtry = best_tune$p,
+                         ntree = best_tune$trees)
   
-  # Evaluate on test set
-  heart_test$rf_predict <- predict(reg_rf, newdata = heart_test)
+  # Test on test data
+  heart_test$test_predict <- predict(rf_fit, newdata=heart_test)
   
-  #Per-class accuracies
-  per_class_accuracy[[i]] <- rep(NA, length(levels(heart_test$Target)))
-  for(l in 1:length(per_class_accuracy[[i]])){
-    per_class_accuracy[[i]][l] <- 
+  # Save fold-specific, class-specific error rates
+  per_class_accuracy <- rep(NA, length(levels(heart_test$Target)))
+  
+  for(l in 1:length(per_class_accuracy)){
+    per_class_accuracy[l] <- 
       heart_test %>%
       filter(Target==levels(Target)[l]) %>%
-      summarise(error = 1-sum(rf_predict==levels(Target)[l])/n()) %>%
+      summarise(accuracy = sum(test_predict==levels(Target)[l])/n()) %>%
       unlist()
     
-    names(per_class_accuracy[[i]])[l] <- 
-      paste0("error_", levels(heart_test$Target)[l])
+    names(per_class_accuracy)[l] <- 
+      paste0("accuracy_", levels(heart_test$Target)[l])
   }
   
+  test_results[[i]] <- per_class_accuracy
 }
-data_set_list  <- as.data.frame(t(do.call("rbind", per_class_accuracy) %>%
-                                    apply(MARGIN=2, FUN=mean, na.rm=TRUE)))
+# Compute CV error estimates and CV SE of estimates
+test_results_all_rf <- data.frame(do.call("rbind", test_results))
+cv_error <- apply(test_results_all_rf, 2, mean)
+cv_error_se <- apply(test_results_all_rf, 2, sd)
+# Create data frame for flex table
+all_results <- data.frame("Heart Disease Category"=levels(derived_heart$Target),
+                          "CV sensitivity/specificity"=cv_error,
+                          "CV sensitivity/specificity SE"=cv_error_se)
+rownames(all_results) <- NULL
+flextable(all_results)
 
-data_set_list %>%
-  flextable() %>% 
-  colformat_num(digits=4) %>%
-  add_header_lines(values="CV ERRORS PER CLASS")
+table2_randomforest <- flextable(all_results) %>% 
+  add_header_lines(values = "Random Forest Sensitivity/specificty")
+ 
 
-data_set_list_se <- as.data.frame(t(do.call("rbind", per_class_accuracy) %>%
-                                      apply(MARGIN=2, FUN=sd, na.rm=TRUE))) 
-
-data_set_list_se %>%
-  flextable() %>% 
-  colformat_num(digits=4) %>%
-  add_header_lines(values="CV ERROR SE PER CLASS")
+save_as_image(table2_randomforest, path = "figures/table2_randomforest.png")
